d4bf9d3a857348f425acf819ba75e6d467fa6711
202100857-art <202100857@p.uapa.edu.do>
2026-07-13 23:12:31 -0400

    Diseñar pantalla de exploración y mapa

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonToolbar,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  standalone: true,
+  imports: [
+    IonContent,
+    IonHeader,
+    IonIcon,
+    IonToolbar,
+  ],
 })
 export class Tab2Page {
-
   constructor() {}
-
 }
